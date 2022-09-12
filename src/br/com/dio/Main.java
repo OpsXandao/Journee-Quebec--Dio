0161829b4e297122bbc92407223181440486b515
Alexandre Paiva <alexandremarcio1504@gmail.com>
2022-09-12 11:31:35 -0300

    First Commit

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.*;
+
+/**
+ * @author Alexandre
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Gato g1 = new Gato();
+		System.out.println(g1);
+		Livros l1 = new Livros();
+		System.out.println(l1);
+	}
+
+
+}
+class Livros{
+	private String nome;}
